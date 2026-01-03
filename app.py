61797f28000e258589e85d881d7ef9d9bd57bb59
Shubham_Bhunia <shubhambhunia917@gmail.com>
2026-01-03 15:13:07 +0530

    Till Now we showing Recent investment of the invester and 5 biggest investment

--- app.py
+++ app.py
@@ -1,8 +1,30 @@
 import streamlit as st
 import pandas as pd
+import matplotlib.pyplot as plt   # For plot graph
+st.set_page_config(layout='wide',page_title = 'Startup Analytics',page_icon='Wallpaper.png')  # For making the layout wider
 df = pd.read_csv('startup_cleaned.csv')
 # st.dataframe(df)
 df['investors'] = df['investors'].fillna('Undisclosed')
+# Load the selected invester name function
+def load_investors_details(investor):
+    st.title(investor)
+    #Load the recent 5 investment of the investor function
+    last5_df = df[df['investors'].str.contains(investor)].head()[['date','startup','vertical','SubVertical','city','round','amount']]
+    st.subheader('Most Recent Investment')
+    st.dataframe(last5_df)
+
+    col1, col2 = st.columns(2)
+    with col1:
+        # Biggest Investment
+        biggest_inv = df[df['investors'].str.contains(investor)].groupby('startup')['amount'].sum().sort_values(
+            ascending=False).head(5)
+        st.subheader('Most biggest Investment')
+        fig, ax = plt.subplots()
+        ax.bar(biggest_inv.index, biggest_inv.values)
+        st.pyplot(fig)
+
+
+
 st.sidebar.title('Startup Funding Analysis')
 option = st.sidebar.selectbox('Select One',['Overall Analysis','Startup','Invester'])
 
@@ -13,6 +35,9 @@ elif option == 'Startup':
     btn1 = st.sidebar.button('Find Startup details')
     st.title('Startup Funding Analysis')
 else:
-    st.sidebar.selectbox('Select Investors',sorted(set(df['investors'].str.split(',').sum())))
+    selected_investors = st.sidebar.selectbox('Select Investors',sorted(set(df['investors'].str.split(',').sum())))
     btn2 = st.sidebar.button('Find Investor details')
-    st.title('Investor Analysis')
+
+    if btn2:
+        load_investors_details(selected_investors)
+    #st.title('Investor Analysis')
