632a97c25171a4fd52236d04bed7922b0a235c9d
Shubham_Bhunia <shubhambhunia917@gmail.com>
2026-01-03 17:08:09 +0530

    Till Now we build the analysis for overall analysis

--- app.py
+++ app.py
@@ -2,14 +2,56 @@ import coerce
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt   # For plot graph
+import time
 
-from Streamlit_doc import date
 
 st.set_page_config(layout='wide',page_title = 'Startup Analytics',page_icon='Wallpaper.png')  # For making the layout wider
 df = pd.read_csv('startup_cleaned.csv')
 df['date'] = pd.to_datetime(df['date'],format='mixed',errors = 'coerce')
 # st.dataframe(df)
 df['investors'] = df['investors'].fillna('Undisclosed')
+
+def load_overall_analysis():
+
+    st.title('Overall Analysis')
+    col1,col2,col3,col4 = st.columns(4)
+    with col1:
+        # Total investment amount
+        total_inv = df['amount'].sum()
+        st.metric('Total Investment', str(int(total_inv)) + ' Cr')
+    with col2:
+        # MAx amount infused in a startup
+        max_inv = df.groupby('startup')['amount'].max().sort_values(ascending=False).head(1).values[0]
+        st.metric('MAximum Investment in a single startup', str(int(max_inv)) + ' Cr')
+    with col3:
+        # Average investment
+        avg_inv = df.groupby('startup')['amount'].sum().mean()
+        st.metric('Average Investment', str(round(avg_inv)) + ' Cr')
+    with col4:
+        # TOtal funded Startup
+        total_startup = df['startup'].nunique()
+        st.metric('Total funded Startups', str(total_startup))
+
+    st.header('Monthly Investment graph')
+
+
+    selected_option = st.selectbox('Select Type',['Total','Count'])
+    df['month'] = df['date'].dt.month
+    df['year'] = df['date'].dt.year
+    if selected_option == 'Total':
+        temp_df = df.groupby(['year', 'month'])['amount'].sum().reset_index()
+    else:
+        temp_df = df.groupby(['year', 'month'])['amount'].count().reset_index()
+
+    temp_df['x_axis'] = temp_df['month'].astype('str') + '-' + temp_df['year'].astype('str')
+    fig6, ax6 = plt.subplots()
+    ax6.plot(temp_df['x_axis'], temp_df['amount'])
+    st.pyplot(fig6)
+
+
+
+
+
 # Load the selected invester name function
 def load_investors_details(investor):
     st.title(investor)
@@ -77,7 +119,8 @@ st.sidebar.title('Startup Funding Analysis')
 option = st.sidebar.selectbox('Select One',['Overall Analysis','Startup','Invester'])
 
 if option == 'Overall Analysis':
-    st.title('Overall Analysis')
+    load_overall_analysis()
+
 elif option == 'Startup':
     st.sidebar.selectbox('Select Startup',sorted(df['startup'].unique().tolist()))
     btn1 = st.sidebar.button('Find Startup details')
